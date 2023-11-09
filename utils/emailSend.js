73be02facaf568cabc1a511c8cf6bb8eea5523a6
Aliyu <sadiqful@gmail.com>
2023-11-09 10:29:38 +0100

    feat: send email

---
+++ utils/emailSend.js
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+const transporter = nodemailer.createTransport({
+  host: process.env.SMTP_HOST,
+  port: process.env.SMTP_PORT,
+  secure: false,
+  auth: {
+    user: process.env.SMTP_USERNAME,
+    pass: process.env.SMTP_PASSWORD,
+  },
+});
+
+export default transporter;
